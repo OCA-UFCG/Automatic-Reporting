e6e8a02bd1a9a5e831a8821a08cda4e6e8920c44
rayaneBSilva <rayane.silva@gmail.com>
2026-08-20 14:15:13 -0300

    fix: bug database

--- utils/database.py
+++ utils/database.py
@@ -4,41 +4,36 @@ from config import DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER
 
 
 def get_connection():
-    try:
-        conexao = psycopg2.connect(
-            host= DB_HOST,
-            database=DB_DATABASE,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            port=DB_PORT,
-            connect_timeout=5
-        )
-        
-        cursor = conexao.cursor()
-        cursor.execute("SELECT version();")
-        versao_db = cursor.fetchone()
-        print("Conexão bem-sucedida!")
-        print(f"Versão do banco de dados: {versao_db[0]}")
-        
-        cursor.close()
-        conexao.close()
-        
-    except psycopg2.OperationalError as e:
-        print(f"Ocorreu um erro operacional (provavelmente rede ou credenciais): {e}")
-    except psycopg2.Error as e:
-        print(f"Erro: {e}")
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_DATABASE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT,
+        connect_timeout=5,
+    )
+
 
 def test_connection():
+    conn = None
+
     try:
         conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT version();")
-        version = cursor.fetchone()
-        cursor.close()
-        conn.close()
+
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT version();")
+            version = cursor.fetchone()
+
+        print("Conexão bem-sucedida!")
+        print(f"Versão do banco de dados: {version[0]}")
         return {"status": "connected", "version": version[0]}
     except psycopg2.Error as e:
+        print(f"Erro na conexão com o banco de dados: {e}")
         return {"status": "error", "message": str(e)}
+    finally:
+        if conn is not None:
+            conn.close()
+
 
 def main():
     test_connection()
